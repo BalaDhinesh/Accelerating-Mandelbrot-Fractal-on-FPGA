9d491d3feed764879be4b75242b572db82bb123b
Jayanth2209 <ee19b026@smail.iitm.ac.in>
2022-05-01 00:02:41 +0530

    Updated Mandebrot Code

--- Baseline_Codes/Baseline_HLS/mandelbrot_tb.cpp
+++ Baseline_Codes/Baseline_HLS/mandelbrot_tb.cpp
@@ -1,23 +1,34 @@
 #include <iostream>
 #include "mandelbrot.h"
+#include <fstream>
 
 using namespace std;
 
 int main () {
-    Config config;
-    config.img_width = 1920;
-    config.img_height = 1080;
-    config.minR = -1.5;
-    config.maxR = 0.5;
-    config.minI = -1.0;
-    config.maxI = 1.0;
-    config.max_iteration = 1000;
+	Config config;
+	    config.img_width = 1920;
+	    config.img_height = 1080;
+	    config.plot_x_min = -2.5;
+	    config.plot_y_max = 1.0;
+	    config.plot_width = 3.5;
+	    config.plot_height = 2.0;
+	    config.max_iteration = 1000;
+
+	    ofstream out_file;
+	    out_file.open("pixel_data.txt");
 
 //    int iteration;
     int out[1920*1080];
 
     mandel(out, config);
-    cout << out[0] << endl;
+    for(int i=0; i<1; i++){
+    	for(int j=0; j<1080; j++){
+    		out_file << out[i*1080+j] << " ";
+    	}
+    	out_file << endl;
+    }
+    // cout << i << "   "<< out[i] << endl;
 
+    out_file.close();
     return 0;
 }
